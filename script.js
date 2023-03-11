1a59ec159a143da485ae38fa50e8d01ed15787eb
aryan04756 <124156524+aryan04756@users.noreply.github.com>
2023-03-12 00:37:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+let string ="";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click',(e)=>{
+    try{
+    if(e.target.innerHTML == '='){
+      string = eval(string)
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""; 
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'X'){
+        string = string.slice(0,-1);
+        document.querySelector('input').value = string;
+    }
+    else {
+    console.log(e.target);
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+    }
+}
+catch(err){
+    console.error('Error:',err.message);
+    alert('Invalid format.');
+    string = ""; 
+    document.querySelector('input').value = string;
+}
+  })
+})
